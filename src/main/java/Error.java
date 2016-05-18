ecc444b093c21dcadda2a025896e25975f87bf34
s <sa>
2016-05-18 14:35:46 -0700

    Add All

--- src/main/java/Error.java
+++ src/main/java/Error.java
@@ -137,4 +137,11 @@ public class Error {
   }
 
 
+  public static int randomNumber() {
+
+    Random random = new Random();
+    int x = random.nextInt(Solution.all().size()-1);
+    //x = a random integer ranging from 0 to the size of my returned list, in our case, 4
+    return x;
+  }
 }
