a92352fe2c36fe885732f2becc34b46eaf21d59b
jf <j.k.fokes@gmail.com>
2016-05-18 15:18:23 -0700

    will change input form to not make mistake of adding multiple tags

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,29 +1,67 @@
-// import org.sql2o.*;
-// import java.util.List;
-// import java.util.ArrayList;
+// // import org.sql2o.*;
+// // import java.util.List;
+// // import java.util.ArrayList;
 
-// public class User {
-//   private int id;
-//   private String error;
-//   private String solution;
+// // public class User {
+// //   private int id;
+// //   private String error;
+// //   private String solution;
 
-//   public User(String error, String solution) {
-//     this.error = error;
-//     this.solution = solution;
-//   }
+// //   public User(String error, String solution) {
+// //     this.error = error;
+// //     this.solution = solution;
+// //   }
 
-//   public int getId() {
-//     return id;
-//   }
-//   public String getError() {
-//     return error;
-//   }
-//   public String getSolution() {
-//     return solution;
-//   }
+// //   public int getId() {
+// //     return id;
+// //   }
+// //   public String getError() {
+// //     return error;
+// //   }
+// //   public String getSolution() {
+// //     return solution;
+// //   }
 
-<<<<<<< HEAD
-//   public List<User> all() {
+// <<<<<<< HEAD
+// //   public List<User> all() {
+// //     String sql = "SELECT id, error, solution FROM userinputs";
+// //     try(Connection con = DB.sql2o.open()) {
+// //       return con.createQuery(sql).executeAndFetch(User.class);
+// //     }
+// //   }
+// //   @Override public boolean equals (Object otherUser) {
+// //     if(!(otherUser instanceof User)) {
+// //       return false;
+// //     } else  {
+// //       User newUser = (User)otherUser;
+// //         return this.getError().equals(newUser.getError()) &&
+// //                this.getSolution().equals(newUser.getSolution) &&
+// //                this.getId() == (newUser.getSolution());
+// //       }
+// //   }
+
+// //   public void save() {
+// //     try(Connection con = DB.sql2o.open()) {
+// //       String sql = "INSERT INTO userinputs(error, solution) WHERE(:error, :solution)";
+// //       this.id = (int) con.createQuery(sql, true)
+// //         .addParameter("error", this.error)
+// //         .addParameter("solution", this.solution)
+// //         .executeUpdate()
+// //         .getKey();
+// //     }
+// //   }
+
+// //   public void delete() {
+// //     try (Connection con = DB.sql2o(open)) {
+// //       String deletequery = "DELETE FROM userinputs WHERE :id > 0";
+// //       con.createQuery(deletequery)
+// //         .addParameter("id", this.id)
+// //         .executeUpdate();
+// //     }
+// //   }
+// // }
+// =======
+//   public static List<User> all() {
 //     String sql = "SELECT id, error, solution FROM userinputs";
 //     try(Connection con = DB.sql2o.open()) {
 //       return con.createQuery(sql).executeAndFetch(User.class);
@@ -35,14 +73,14 @@
 //     } else  {
 //       User newUser = (User)otherUser;
 //         return this.getError().equals(newUser.getError()) &&
-//                this.getSolution().equals(newUser.getSolution) &&
-//                this.getId() == (newUser.getSolution());
+//                this.getSolution().equals(newUser.getSolution()) &&
+//                this.getId() == (newUser.getId());
 //       }
 //   }
 
 //   public void save() {
 //     try(Connection con = DB.sql2o.open()) {
-//       String sql = "INSERT INTO userinputs(error, solution) WHERE(:error, :solution)";
+//       String sql = "INSERT INTO userinputs(error, solution) VALUES(:error, :solution)";
 //       this.id = (int) con.createQuery(sql, true)
 //         .addParameter("error", this.error)
 //         .addParameter("solution", this.solution)
@@ -52,7 +90,7 @@
 //   }
 
 //   public void delete() {
-//     try (Connection con = DB.sql2o(open)) {
+//     try (Connection con = DB.sql2o.open()) {
 //       String deletequery = "DELETE FROM userinputs WHERE :id > 0";
 //       con.createQuery(deletequery)
 //         .addParameter("id", this.id)
@@ -60,42 +98,4 @@
 //     }
 //   }
 // }
-=======
-  public static List<User> all() {
-    String sql = "SELECT id, error, solution FROM userinputs";
-    try(Connection con = DB.sql2o.open()) {
-      return con.createQuery(sql).executeAndFetch(User.class);
-    }
-  }
-  @Override public boolean equals (Object otherUser) {
-    if(!(otherUser instanceof User)) {
-      return false;
-    } else  {
-      User newUser = (User)otherUser;
-        return this.getError().equals(newUser.getError()) &&
-               this.getSolution().equals(newUser.getSolution()) &&
-               this.getId() == (newUser.getId());
-      }
-  }
-
-  public void save() {
-    try(Connection con = DB.sql2o.open()) {
-      String sql = "INSERT INTO userinputs(error, solution) VALUES(:error, :solution)";
-      this.id = (int) con.createQuery(sql, true)
-        .addParameter("error", this.error)
-        .addParameter("solution", this.solution)
-        .executeUpdate()
-        .getKey();
-    }
-  }
-
-  public void delete() {
-    try (Connection con = DB.sql2o.open()) {
-      String deletequery = "DELETE FROM userinputs WHERE :id > 0";
-      con.createQuery(deletequery)
-        .addParameter("id", this.id)
-        .executeUpdate();
-    }
-  }
-}
->>>>>>> dbe5a8946678a96a383dbc6c3d2bf71692a5a3e9
+// >>>>>>> dbe5a8946678a96a383dbc6c3d2bf71692a5a3e9
